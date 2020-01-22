89f792b0bb4f42be8c60e95b8a50989addd8396d
yunue <1934182494@qq.com>
2020-01-22 20:29:46 +0800

    new version
    add smooth animations

--- snake1.2.py
+++ snake1.2.py
@@ -4,15 +4,16 @@ from pygame.locals import *
 pygame.init()
 
 #             R   G   B
-RED   =     (255,  0, 25)
+RED   =     (255,  0, 40)
 DEEPGRAY  = ( 25, 25, 25)
 GRAY =      ( 40, 40, 40)
-WHITE =     (255,255,255)
+WHITE =     (240,240,255)
 
 UP = 0
 DOWN = 1
 LEFT = 2
 RIGHT = 3
+FPS = 60
 
 faces = [[UP,UP,LEFT,RIGHT],
          [DOWN,DOWN,LEFT,RIGHT],
@@ -20,9 +21,13 @@ faces = [[UP,UP,LEFT,RIGHT],
          [UP,DOWN,RIGHT,RIGHT]
         ]
 bodys = []
+towards = []
 HEAD = 0
 direction = UP
 new_direction = UP
+head_animation = True
+
+level = 10   #biger,easier
 apple = {}
 old_apple = {}
 BODY_SIZE = 20
@@ -33,7 +38,7 @@ fpsLim = 20
 counter = fpsLim
 fps = pygame.time.Clock()
 screen = pygame.display.set_mode((BODY_SIZE*BOARD_LONG,BODY_SIZE*BOARD_WIDE))
-pygame.display.set_caption('Snake')
+pygame.display.set_caption('Snake 1.2')
 
 fontObj = pygame.font.SysFont("Impact",256)
 
@@ -44,17 +49,18 @@ def main():
         terminal()
 
 def set_up():
-    global bodys,direction
+    global bodys,direction,towards
     x = random.randint(2,BOARD_LONG - 2)
     y = random.randint(2,BOARD_WIDE - 2)
     bodys = [{'x':x ,'y':y    },
              {'x':x ,'y':y + 1},
              {'x':x ,'y':y + 2}]
+    towards = [UP,UP,UP]
     direction = UP
     get_apple()
 
 def main_loop():
-    global counter,screen,bodys,apple,direction,new_direction,fpsLim
+    global counter,screen,bodys,apple,direction,new_direction,fpsLim,towards
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -89,25 +95,66 @@ def main_loop():
                 get_apple()
             else:
                 del bodys[-1]
+                del towards[-1]
+                
             bodys.insert(0,new_head)
+            towards.insert(0,new_direction)
             counter = fpsLim
             direction = new_direction
-            fpsLim = 20 - 2 * (len(bodys)//5)
+            fpsLim = 20 - 2 * (len(bodys)//level)
         else:
             counter -= 1
 
         screen.fill(DEEPGRAY)
         score_board()
-        for rect in bodys:
+
+        draw_head(bodys[HEAD],counter)
+        draw_tail(bodys[-1],counter)
+        
+        for rect in bodys[1:]:
             draw_body(rect)
         draw_apple(apple,counter)
 
         pygame.display.update()
-        fps.tick(60)
+        fps.tick(FPS)
 
 def draw_body(rect):
     pygame.draw.rect(screen,WHITE,((rect['x']-1)*BODY_SIZE,(rect['y']-1)*BODY_SIZE,BODY_SIZE,BODY_SIZE))
 
+def draw_tail(rect,counter):
+    global bodys,head_animation,towards
+    if head_animation == True:
+        if towards[-1] == DOWN:  #go downside
+            pygame.draw.rect(screen,WHITE,((rect['x']-1)*BODY_SIZE,(rect['y']-1)*BODY_SIZE - counter,BODY_SIZE,BODY_SIZE))
+        elif towards[-1] == UP:  #go upside
+            pygame.draw.rect(screen,WHITE,((rect['x']-1)*BODY_SIZE,(rect['y']-1)*BODY_SIZE + counter,BODY_SIZE,BODY_SIZE))
+        elif towards[-1] == LEFT:  #go left
+            pygame.draw.rect(screen,WHITE,((rect['x']-1)*BODY_SIZE + counter,(rect['y']-1)*BODY_SIZE,BODY_SIZE,BODY_SIZE))
+        elif towards[-1] == RIGHT:  #go right
+            pygame.draw.rect(screen,WHITE,((rect['x']-1)*BODY_SIZE - counter,(rect['y']-1)*BODY_SIZE ,BODY_SIZE,BODY_SIZE))
+    else:
+        pygame.draw.rect(screen,WHITE,((rect['x']-1)*BODY_SIZE,(rect['y']-1)*BODY_SIZE,BODY_SIZE,BODY_SIZE))
+            
+def draw_head(rect,counter):
+    global bodys,head_animation
+
+    if head_animation == True:
+        if direction == UP:
+            pygame.draw.rect(screen,WHITE,((rect['x']-1)*BODY_SIZE,(rect['y']-1)*BODY_SIZE + counter,BODY_SIZE,BODY_SIZE))
+        elif direction == DOWN:
+            pygame.draw.rect(screen,WHITE,((rect['x']-1)*BODY_SIZE,(rect['y']-1)*BODY_SIZE - counter,BODY_SIZE,BODY_SIZE))
+        elif direction == LEFT:
+            pygame.draw.rect(screen,WHITE,((rect['x']-1)*BODY_SIZE + counter,(rect['y']-1)*BODY_SIZE,BODY_SIZE,BODY_SIZE))
+        elif direction == RIGHT:
+            pygame.draw.rect(screen,WHITE,((rect['x']-1)*BODY_SIZE - counter,(rect['y']-1)*BODY_SIZE ,BODY_SIZE,BODY_SIZE))
+            
+        if counter == 6:
+            head_animation = False
+    else:
+        pygame.draw.rect(screen,WHITE,((rect['x']-1)*BODY_SIZE,(rect['y']-1)*BODY_SIZE,BODY_SIZE,BODY_SIZE))
+        if counter == 0:
+            head_animation = True
+            
 def draw_apple(rect,counter):
     global apple,old_apple
 
@@ -140,3 +187,4 @@ def score_board():
     screen.blit(boardSurfaceobj,scoreBoard)
 
 if __name__ == "__main__":main()
+
